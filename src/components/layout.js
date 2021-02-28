cc67ba9ec97596eddcbad9dc08ca3a0120d85f50
allthesignals <wilburnforce@gmail.com>
2021-02-28 05:27:51 -0500

    Relink netlify build

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,7 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 
-const Layout = ({ children, loaded = true }) => {
+const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
